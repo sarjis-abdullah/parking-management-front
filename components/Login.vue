6e134e2dfa8ba408b3bf5137883548977b1f6e78
Sarjis Abdullah <55045212+sarjis-abdullah@users.noreply.github.com>
2024-04-26 12:55:14 +0600

    login updated

--- components/Login.vue
+++ components/Login.vue
@@ -1,68 +1,86 @@
 <template>
-  <div class="p-4 max-w-lg mx-auto shadow-2xl">
-    <section class="p-4">
-      <form
-        v-if="!regFormSubmitted"
-        @submit.prevent="submitForm"
-        class="grid gap-2"
-      >
-        <!-- Phone -->
-        <div class="grid gap-2" :style="style">
-          <label for="phone" class="block font-bold">Phone</label>
-          <input
-            type="text"
-            id="phone"
-            v-model="formData.phone"
-            :class="inputClass"
-            placeholder="e.g. +880123456789"
-            required
-          />
-          <span v-if="errors.phone" class="text-red-500">{{
-            errors.phone
-          }}</span>
-        </div>
-
-        <!-- Email -->
-        <div class="grid gap-2" :style="style">
-          <label for="email" class="block font-bold">Password</label>
-          <input
-            type="password"
-            id="email"
-            v-model="formData.password"
-            :class="inputClass"
-          />
-          <span v-if="errors.email" class="text-red-500">{{
-            errors.email
-          }}</span>
-        </div>
-
-        <button
-          type="submit"
-          v-if="!loading"
-          class="bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600 mt-4"
+  <section class="flex items-center justify-center h-screen">
+    <div class="p-4 w-[28rem] mx-auto shadow-2xl">
+      <!-- Lorem ipsum dolor sit amet consectetur adipisicing elit. Vitae repellendus facere temporibus culpa officiis suscipit error architecto iusto, ullam harum? -->
+      <section class="p-4">
+        <form
+          v-if="!regFormSubmitted"
+          @submit.prevent="submitForm"
+          class="grid gap-2"
         >
-          Login
-        </button>
-        <span v-else>Processing</span>
-      </form>
-    </section>
-  </div>
+          <!-- email -->
+          <div class="grid gap-2" :style="style">
+            <label for="email" class="block font-bold">Email</label>
+            <input
+              type="email"
+              id="email"
+              v-model="formData.email"
+              :class="inputClass"
+              placeholder="e.g. test@mart.com"
+              required
+            />
+            <span v-if="errors.email" class="text-red-500">{{
+              errors.email
+            }}</span>
+          </div>
+
+          <!-- Email -->
+          <div class="grid gap-2" :style="style">
+            <label for="email" class="block font-bold">Password</label>
+            <input
+              type="password"
+              id="email"
+              v-model="formData.password"
+              :class="inputClass"
+            />
+            <span v-if="errors.email" class="text-red-500">{{
+              errors.email
+            }}</span>
+          </div>
+
+          <button
+            type="submit"
+            v-if="!loading"
+            class="bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600 mt-4"
+          >
+            Login
+          </button>
+          <span v-else>Processing</span>
+        </form>
+      </section>
+      <div v-if="errors">
+        <div
+          v-for="error in errors"
+          :key="error"
+          class="text-center text-red-500"
+        >
+          {{ error }}
+        </div>
+      </div>
+    </div>
+  </section>
 </template>
 
 <script setup>
 import { ref } from "vue";
+import Default from "../layouts/Default.vue";
+import Login from "../components/Login.vue";
 // import AccountStorage from "../storage/Index";
 
+definePageMeta({
+  // layout: "Default",
+});
 const route = useRoute();
+const router = useRouter();
 
 const config = useRuntimeConfig();
-const url = config.public.BASE_URL + "user/login";
+const url = config.public.BASE_URL + "login";
 const style = "";
 const inputClass =
   "relative block w-full appearance-none rounded-md border border-gray-300 px-3 py-2 text-gray-900 placeholder-gray-500 focus:z-10 focus:outline-none focus:ring-blue-500 sm:text-sm focus:border-blue-500";
 const defaultData = {
   password: "",
-  phone: "",
+  email: "",
 };
 const formData = ref({ ...defaultData });
 
@@ -72,6 +90,7 @@ const regFormSubmitted = ref(false);
 const success = ref(false);
 const userId = ref(null);
 const otp = ref("");
+const isLogin = ref(false);
 
 const submitForm = () => {
   const options = {
@@ -96,16 +115,20 @@ const submitForm = () => {
     })
     .then((data) => {
       loading.value = false;
-      console.log(data, 12345);
       if (data.user) {
-        localStorage.setItem('LOGIN_ACCOUNT',JSON.stringify(data.user));
-        // AccountStorage.saveAccount(data.user);
+        localStorage.setItem("LOGIN_ACCOUNT", JSON.stringify(data.user));
+        localStorage.setItem("ACCESS_TOKEN", data.accessToken);
+        isLogin.value = true;
+        router.push("/customer-list");
+        // Reload the page
+        // window.location.reload();
       }
     })
     .catch((error) => {
       setTimeout(() => {
         loading.value = false;
       }, 1000);
+      console.error("Error:", error, error.message, 2345);
       // Handle error from the server or network
     });
 };
