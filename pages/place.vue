a3c45e4f6585df16737922ed3516dd5273ebee14
Sarjis Abdullah <55045212+sarjis-abdullah@users.noreply.github.com>
2024-04-26 20:24:02 +0600

    example place schema added

--- pages/place.vue
+++ pages/place.vue
@@ -12,39 +12,46 @@ import AuthLayout from "../layouts/AuthLayout.vue";
 
 <script setup>
 import AuthLayout from "../layouts/AuthLayout.vue";
-import { Form, Field, ErrorMessage, } from 'vee-validate'
-import * as yup from 'yup'
-import { ref } from 'vue'
-const formRef = ref(null)
+import storeSchema from "../validationSchema/place/storeSchema";
+import { Form, Field, ErrorMessage } from "vee-validate";
+import * as yup from "yup";
+import { ref } from "vue";
+const formRef = ref(null);
 
-const schema = yup.object().shape({
-  email: yup
-    .string()
-    .email('Email is not valid')
-    .required((data) => `${data.path} harus diisi`),
-  password: yup
-    .string()
-    .min(5, (data) => `Password minimal ${data.min} karakter`)
-    .required((data) => `${data.label} harus diisi`)
-    .label('Password'),
-})
+const schema = storeSchema
 const onSubmit = (values) => {
-  console.log(values)
-  formRef.value?.resetForm()
-
-}
+  console.log(values);
+  formRef.value?.resetForm();
+};
+const inputClass =
+  "relative block w-full appearance-none rounded-md border border-gray-300 px-3 py-2 text-gray-900 placeholder-gray-500 focus:z-10 focus:outline-none focus:ring-blue-500 sm:text-sm focus:border-blue-500";
 </script>
 
 <template>
   <AuthLayout>
-    <Form v-slot="{ values, handleReset }" ref="formRef" @submit="onSubmit" :validation-schema="schema">
-    <Field name="email" placeholder="email" />
-    <ErrorMessage name="email" />
-    <Field class="relative block w-full appearance-none rounded-md border border-gray-300 px-3 py-2 text-gray-900 placeholder-gray-500 focus:z-10 focus:outline-none focus:ring-blue-500 sm:text-sm focus:border-blue-500'" name="password" placeholder="password" />
-    <ErrorMessage name="password" />
-    <button type="submit">Submit</button>
-    <button type="button" @click="handleReset">Reset</button>
-
-  </Form>
+    <section class="max-w-96">
+      <Form
+        v-slot="{ values, handleReset }"
+        ref="formRef"
+        @submit="onSubmit"
+        :validation-schema="schema"
+        class="grid gap-3"
+      >
+        <div class="grid gap-2">
+          <label class="text-gray-500">Email</label>
+          <Field :class="inputClass" name="email" placeholder="email" />
+          <ErrorMessage class="text-red-500" name="email" />
+        </div>
+        <div>
+          <label class="text-gray-500">Password</label>
+          <Field :class="inputClass" name="password" placeholder="password" />
+          <ErrorMessage class="text-red-500" name="password" />
+        </div>
+        <div class="flex justify-end gap-2">
+          <button type="submit" class="bg-indigo-600 text-white px-2 py-1 rounded-md">Submit</button>
+          <button type="button" @click="handleReset">Reset</button>
+        </div>
+      </Form>
+    </section>
   </AuthLayout>
 </template>
