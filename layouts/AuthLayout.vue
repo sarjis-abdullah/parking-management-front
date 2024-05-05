f19bb3cf51ab0598a55159a562fa3c0ae8bf034a
Sarjis Abdullah <55045212+sarjis-abdullah@users.noreply.github.com>
2024-05-05 10:56:14 +0600

    un used code removed

--- layouts/AuthLayout.vue
+++ layouts/AuthLayout.vue
@@ -187,35 +187,6 @@
                 <SingleNav v-for="item in navigation" :key="item.name" :item="item"></SingleNav>
               </ul>
             </li>
-            <li>
-              <div class="text-xs font-semibold leading-6 text-gray-400">
-                Your teams
-              </div>
-              <ul role="list" class="-mx-2 mt-2 space-y-1">
-                <li v-for="team in teams" :key="team.name">
-                  <a
-                    :href="team.href"
-                    :class="[
-                      team.current
-                        ? 'bg-gray-50 text-indigo-600'
-                        : 'text-gray-700 hover:text-indigo-600 hover:bg-gray-50',
-                      'group flex gap-x-3 rounded-md p-2 text-sm leading-6 font-semibold',
-                    ]"
-                  >
-                    <span
-                      :class="[
-                        team.current
-                          ? 'text-indigo-600 border-indigo-600'
-                          : 'text-gray-400 border-gray-200 group-hover:border-indigo-600 group-hover:text-indigo-600',
-                        'flex h-6 w-6 shrink-0 items-center justify-center rounded-lg border text-[0.625rem] font-medium bg-white',
-                      ]"
-                      >{{ team.initial }}</span
-                    >
-                    <span class="truncate">{{ team.name }}</span>
-                  </a>
-                </li>
-              </ul>
-            </li>
             <li class="mt-auto">
               <a
                 href="#"
@@ -377,7 +348,7 @@ const rawNavigation = ref([
     name: "User",
     href: "/user",
     icon: UsersIcon,
-    show: true,
+    show: false,
     children: [
       { name: "Add", href: "/add/user" },
       { name: "List", href: "/user" },
@@ -394,7 +365,7 @@ const rawNavigation = ref([
     ],
   },
   {
-    name: "Vehicle Category",
+    name: "Category",
     href: "/category",
     icon: DocumentDuplicateIcon,
     show: false,
@@ -447,9 +418,9 @@ const rawNavigation = ref([
 ]);
 const navigation = computed(() => {
   return rawNavigation.value.map((item) => {
-    console.log(route.name, item.href);
+    // console.log(route.name, item.href);
     const href = item.href.split("/")[1];
-    console.log(href);
+    // console.log(href);
     return {
       ...item,
       // show: false,
