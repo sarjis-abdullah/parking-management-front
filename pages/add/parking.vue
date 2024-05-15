ed1c6ff16b3e97453a27a1997a5fc3190104f0a3
Sarjis Abdullah <55045212+sarjis-abdullah@users.noreply.github.com>
2024-05-15 23:33:21 +0600

    floor added

--- pages/add/parking.vue
+++ pages/add/parking.vue
@@ -9,6 +9,7 @@ import { ParkingService } from "@/services/ParkingService.js";
 import { PlaceService } from "@/services/PlaceService.js";
 import { SlotService } from "~/services/SlotService";
 import { CategoryService } from "~/services/CategoryService";
+import { FloorService } from "~/services/FloorService";
 
 definePageMeta({
   layout: "auth-layout",
@@ -20,6 +21,7 @@ const defaultData = {
   place: "",
   slot: "",
   category: "",
+  floor: "",
 };
 const serverErrors = ref({});
 const state = reactive(defaultData);
@@ -36,6 +38,9 @@ const rules = computed(() => {
     category: {
       required: helpers.withMessage("category is required", required),
     },
+    floor: {
+      required: helpers.withMessage("floor is required", required),
+    },
     slot: {
       required: helpers.withMessage("slot is required", required),
     },
@@ -44,12 +49,13 @@ const rules = computed(() => {
 const validator = useVuelidate(rules, state, { $lazy: true });
 
 const loading = ref(false);
-
+const selectedSlot = ref();
 const handleReset = async () => {
   await validator.value.$reset();
   for (let key in state) {
     state[key] = "";
   }
+  selectedSlot.value = null
   // formRef.value?.reset();
   console.log("handleReset");
 };
@@ -61,16 +67,17 @@ const parkingData = computed(() => {
     place_id: state.place,
     category_id: state.category,
     slot_id: state.slot,
+    floor_id: state.floor,
   };
 });
-const router = useRouter()
+const router = useRouter();
 
 const postItem = async () => {
   try {
     loading.value = true;
     const response = await ParkingService.create(parkingData.value);
     if (response?.data?.barcode) {
-      router.push('/parking-checkin/' + response.data.barcode)
+      router.push("/parking-checkin/" + response.data.barcode);
     }
     serverErrors.value = {};
     handleReset();
@@ -96,21 +103,59 @@ const getPlaces = async () => {
   places.value = data;
 };
 const slots = ref([]);
+
 const getSlots = async () => {
-  const { data } = await SlotService.getAll(`?category_id=${state.category}`);
-  slots.value = data;
+  // const { data } = await SlotService.getAll(`?include=s.floor&category_id=${state.category}`);
+  // slots.value = data;
 };
 const categories = ref([]);
 const getCategories = async () => {
+  state.category = ''
   const { data } = await CategoryService.getAll(`?place_id=${state.place}`);
   categories.value = data;
 };
+const floors = ref([]);
+
+const getFloors = async () => {
+  let query = `?include=f.slots`
+  if(state.place){
+    query += `&place_id=${state.place}`
+  }
+  const { data } = await FloorService.getAll(query);
+  floors.value = data;
+};
 const handlePlaceChange = () => {
   getCategories();
-  // getFloors()
+  getFloors();
 };
+const getSlotClasses = (slot)=> {
+  let className = ''
+  if (slot.status == 'available') {
+    if (selectedSlot.value && selectedSlot.value.id == slot.id) {
+      className = 'bg-blue-500'
+    }else {
+      className = 'bg-green-500'
+    }
+    className += ' cursor-pointer'
+  }else {
+    className = 'bg-yellow-500 pointer-events-none cursor-not-allowed'
+  }
+  return className
+}
+const handleSelectedSlot = (slot)=> {
+  if(selectedSlot.value && selectedSlot.value.id == slot.id){
+    selectedSlot.value = null
+    state.slot = null
+    state.floor = null
+  }else {
+    selectedSlot.value = slot
+      state.slot = slot.id
+      state.floor = slot.floor_id
+  }
+}
 onMounted(() => {
   getPlaces();
+  getFloors()
 });
 
 const inputClass =
@@ -178,25 +223,6 @@ const inputClass =
           <ErrorMessage :errors="validator.category.$errors" />
         </div>
         <div class="grid gap-2">
-          <label class="text-gray-500"
-            >Slot<span class="text-red-500">*</span></label
-          >
-          <select
-            class="focus:outline-none bg-none"
-            :class="inputClass"
-            style="background: none"
-            name="place"
-            v-model="state.slot"
-          >
-            <option disabled :value="''">Select a slot</option>
-            <option v-for="slot in slots" :key="slot.id" :value="slot.id">
-              {{ slot.name }}
-            </option>
-            <!-- Add more options as needed -->
-          </select>
-          <ErrorMessage :errors="validator.slot.$errors" />
-        </div>
-        <div class="grid gap-2">
           <label class="text-gray-500">Driver Name</label>
           <input
             :class="inputClass"
@@ -216,6 +242,43 @@ const inputClass =
           />
           <ErrorMessage :errors="validator.driverMobile.$errors" />
         </div>
+        <div class="grid gap-2">
+          <label class="text-gray-500"
+            >Slot<span class="text-red-500">*</span></label
+          >
+          <ul class="grid gap-2">
+            <li v-for="(floor, index) in floors" :key="floor.id" >
+              <ul class="grid grid-cols-6 gap-2">
+                <li v-for="(slot, i) in floor.slots" :key="slot.id" class="flex flex-col gap-1 items-center p-4 border text-white rounded-md" :class="getSlotClasses(slot)" @click="handleSelectedSlot(slot)">
+                  <div>{{ slot.name }}</div>
+                  <div>
+                    <svg class="h-6 w-6" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+                      <title>car-select</title>
+                      <path
+                      fill="white"
+                        d="M5 13L6.5 8.5H17.5L19 13M17.5 18C16.7 18 16 17.3 16 16.5S16.7 15 17.5 15 19 15.7 19 16.5 18.3 18 17.5 18M6.5 18C5.7 18 5 17.3 5 16.5S5.7 15 6.5 15 8 15.7 8 16.5 7.3 18 6.5 18M18.9 8C18.7 7.4 18.1 7 17.5 7H6.5C5.8 7 5.3 7.4 5.1 8L3 14V22C3 22.6 3.4 23 4 23H5C5.6 23 6 22.6 6 22V21H18V22C18 22.6 18.4 23 19 23H20C20.6 23 21 22.6 21 22V14M8 1L12 5.5L16 1Z"
+                      />
+                    </svg>
+                  </div>
+                </li>
+              </ul>
+            </li>
+          </ul>
+          <!-- <select
+            class="focus:outline-none bg-none"
+            :class="inputClass"
+            style="background: none"
+            name="place"
+            v-model="state.slot"
+          >
+            <option disabled :value="''">Select a slot</option>
+            <option v-for="slot in slots" :key="slot.id" :value="slot.id">
+              {{ slot.name }}
+            </option>
+          </select> -->
+          <ErrorMessage :errors="validator.slot.$errors" />
+        </div>
+        
       </section>
       <ul>
         <li v-for="item in serverErrors" :key="item">
