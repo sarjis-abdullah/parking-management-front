0fc318d81b2430f3c76a67c8132cbead69a3bded
Sarjis Abdullah <55045212+sarjis-abdullah@users.noreply.github.com>
2024-09-13 13:04:18 +0600

    updated

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,8 +2,10 @@
 export default defineNuxtConfig({
   runtimeConfig: {
     BASE_URL: process.env.BASE_URL,
+    APP_URL: process.env.APP_URL,
     public: {
       BASE_URL: process.env.BASE_URL,
+      APP_URL: process.env.APP_URL,
       apiBase: "", // can be overridden by NUXT_PUBLIC_API_BASE environment variable
     },
   },
@@ -20,5 +22,10 @@ export default defineNuxtConfig({
   },
   modules: [
     'nuxt-particles'
-  ]
+  ],
+  devServer: {
+    // for check in mbl device
+    port: 3000, // default: 3000
+    host: '0.0.0.0', // default: localhost
+  },
 });
